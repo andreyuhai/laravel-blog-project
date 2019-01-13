028869f3fa7e418f4f1d5777afb54fb4cc71e8a4
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-01-13 19:44:40 +0100

    Now submit button on create post page should clear the title_content from local storage

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,6 +1,11 @@
 const title = document.getElementById('title')
-title.value = localStorage.getItem("title"); // get and assign the value outside
+title.value = localStorage.getItem("title") // get and assign the value outside
 title.onchange = function () {
-    const new_title = title.value;
-    localStorage.setItem('title', new_title);
+    const new_title = title.value
+    localStorage.setItem('title', new_title)
+}
+
+const submitButton = document.getElementsByClassName('btn btn-primary')
+submitButton.onclick = function () {
+    localStorage.setItem('title_content', "")
 }
