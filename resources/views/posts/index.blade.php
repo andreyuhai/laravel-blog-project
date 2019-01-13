60b745becdcb5f30a1c88bc400307d15d8ec70bf
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-01-13 19:45:16 +0100

    Now if user does not updates an image, noimage.jpg is shown

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,22 +7,26 @@
 @section('content')
     <h1 class="text-center">Posts</h1>
     @if(count($posts) > 0)
-            <div class="card-columns">
+        <div class="card-columns">
             @foreach($posts as $post)
-                        <div class="card">
-                            <img class="card-img-top"
-                                 src="{{url('/storage/cover_images/'.$post->user_id.'/'.$post->cover_image)}}"
-                                 alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title"><a href="{{url('/posts/'.$post->id)}}">{{$post->title}}</a></h5>
-                                <p class="card-text">{!!str_limit($post->body, 200)!!}</p>
-                            </div>
-                            <div class="card-footer">
-                                <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
-                            </div>
-                        </div>
+                <div class="card">
+                    <img class="card-img-top"
+                         @if($post->cover_image == 'noimage.jpg')
+                                src="{{url('/storage/cover_images/noimage.jpg')}}"
+                         @else
+                                src="{{url('/storage/cover_images/'.$post->user_id.'/'.$post->cover_image)}}"
+                         @endif
+                         alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="{{url('/posts/'.$post->id)}}">{{$post->title}}</a></h5>
+                        <p class="card-text">{!!str_limit($post->body, 200)!!}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+                    </div>
+                </div>
             @endforeach
-            </div>
+        </div>
         {{$posts->links()}}
     @else
         <p>No posts found.</p>
