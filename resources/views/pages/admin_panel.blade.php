de54fa56a30f6f94bec3d459df6394e7dee42702
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2019-01-14 02:26:56 +0100

    Table for users added

--- resources/views/pages/admin_panel.blade.php
+++ resources/views/pages/admin_panel.blade.php
@@ -6,5 +6,28 @@
 
 @section('content')
     <h1>{{$title}}</h1>
-    <p>This is the admin panel page.</p>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Post Count</th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        @foreach($users as $user)
+            <tr>
+                <th scope="row">{{$loop->iteration}}</th>
+                <th scope="row">{{$user->name}}</th>
+                <th scope="row">{{$user->email}}</th>
+                <th scope="row">{{$user->posts()->count()}}</th>
+                <th scope="row">Delete user</th>
+            </tr>
+        @endforeach
+        <tbody>
+
+        </tbody>
+    </table>
+    {{$users->links()}}
 @endsection
